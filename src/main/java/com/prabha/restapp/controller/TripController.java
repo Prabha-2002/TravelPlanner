0020ecfdb50ff25577c1dadd3142b832af3a2784
Prabha-2002 <163090094+Prabha-2002@users.noreply.github.com>
2024-08-28 22:56:20 +0530

    Update TripController.java

--- src/main/java/com/prabha/restapp/controller/TripController.java
+++ src/main/java/com/prabha/restapp/controller/TripController.java
@@ -1,66 +1,3 @@
-//package com.prabha.restapp.controller;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.web.bind.annotation.*;
-//
-//import com.prabha.restapp.model.Trip;
-//import com.prabha.restapp.serviceimplementation.TripServiceImpl;
-//
-//import java.util.List;
-//
-//@RestController
-//@RequestMapping("/api/trips")
-//@CrossOrigin("*")
-//
-//public class TripController {
-//
-////    @Autowired
-////    private TripServiceImpl tripService;
-////
-////    @PostMapping("/add")
-////    public Trip createTrip(@RequestBody Trip trip) {
-////        return tripService.saveTrip(trip);
-////    }
-////
-////    @GetMapping("/get")
-////    public List<Trip> getAllTrips() {
-////        return tripService.getAllTrips();
-////    }
-////    @GetMapping("/location/{location}")
-////    public Trip getTripByLocation(@PathVariable String location) {
-////        return tripService.findByLocation(location);
-////    }
-//	
-//	
-//	  @Autowired
-//	   private  TripServiceImpl tripService;
-//
-//	    @PostMapping("/add")
-//	    public Trip createTrip(@RequestBody Trip trip) {
-//	        return tripService.saveTrip(trip);
-//	    }
-//
-//	    @GetMapping("/location/{location}")
-//	    public Trip getTripByLocation(@PathVariable String location) {
-//	        return tripService.findByLocation(location);
-//	    }
-//
-//	    @GetMapping("/{tripId}/places")
-//	    public List<Place> getPlaces(@PathVariable Long tripId) {
-//	        return tripService.getPlacesByTripId(tripId);
-//	    }
-//
-//	    @GetMapping("/{tripId}/hotels")
-//	    public List<TopPortal> getHotels(@PathVariable Long tripId) {
-//	        return tripService.getHotelsByTripId(tripId);
-//	    }
-//
-//	    @GetMapping("/all")
-//	    public List<Trip> getAllTrips() {
-//	        return tripService.getAllTrips();
-//	    }
-//
-//}
 package com.prabha.restapp.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -93,17 +30,17 @@ public class TripController {
 
     @GetMapping("/{tripId}/places")
     public List<Place> getPlaces(@PathVariable Long tripId) {
-        return tripService.getPlacesByTripId(tripId); // Ensure this method is present in TripService
+        return tripService.getPlacesByTripId(tripId);
     }
 
     @GetMapping("/{tripId}/hotels")
     public List<TopPortal> getHotels(@PathVariable Long tripId) {
-        return tripService.getHotelsByTripId(tripId); // Ensure this method is present in TripService
+        return tripService.getHotelsByTripId(tripId); 
     }
 
     @GetMapping("/all")
     public List<Trip> getAllTrips() {
-        return tripService.getAllTrips(); // Ensure this method is present in TripService
+        return tripService.getAllTrips(); 
     }
 }
 
