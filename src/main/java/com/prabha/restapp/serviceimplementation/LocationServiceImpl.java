9926491b34c46379362a1b097c484cc8451385dc
Prabha-2002 <163090094+Prabha-2002@users.noreply.github.com>
2024-08-29 10:29:17 +0530

    Update LocationServiceImpl.java

--- src/main/java/com/prabha/restapp/serviceimplementation/LocationServiceImpl.java
+++ src/main/java/com/prabha/restapp/serviceimplementation/LocationServiceImpl.java
@@ -1,46 +1,4 @@
-//package com.prabha.restapp.serviceimplementation;
-//
-//import com.prabha.restapp.model.Location;
-//import com.prabha.restapp.repo.LocationRepository;
-//import com.prabha.restapp.service.LocationService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Service;
-//
-//import java.util.List;
-//
-//@Service
-//public class LocationServiceImpl implements LocationService {
-//
-//    @Autowired
-//    private LocationRepository locationRepository;
-//
-//    @Override
-//    public Location addLocation(Location location) {
-//        return locationRepository.addLocation(location);
-//    }
-//
-//    @Override
-//    public Location updateLocation(Long id, Location location) {
-//        return locationRepository.updateLocation(id, location);
-//    }
-//
-//    @Override
-//    public void deleteLocation(Long id) {
-//        locationRepository.deleteLocation(id);
-//    }
-//
-//    @Override
-//    public Location getLocationById(Long id) {
-//        return locationRepository.getLocationById(id);
-//    }
-//
-//    @Override
-//    public List<Location> getAllLocations() {
-//        return locationRepository.getAllLocations();
-//    }
-//}
 package com.prabha.restapp.serviceimplementation;
-
 import com.prabha.restapp.model.Location;
 import com.prabha.restapp.repo.LocationRepository;
 import com.prabha.restapp.service.LocationService;
@@ -89,12 +47,10 @@ public class LocationServiceImpl implements LocationService {
 		 }
 
 	public Location findById1(Long id) {
-		// TODO Auto-generated method stub
         return getLocationById(id);
 	}
 
 	public Location findById(String location) {
-		// TODO Auto-generated method stub
 		return locationRepository.getLocationById(null);
 	}
 }
